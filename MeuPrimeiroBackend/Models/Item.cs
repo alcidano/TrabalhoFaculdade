14c9cf5134c8ceaba90a82241f424c0bdd41bebf
MaThEuS fElIpE <matheusmoreiraferreira@hotmail.com>
2017-11-05 22:43:54 -0200

    Trabalho Unity Minotauro

---
+++ MeuPrimeiroBackend/Models/Item.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MeuPrimeiroBackend.Models
+{
+    public class Item
+    {
+        public int ItemID { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Descricao { get; set; }
+
+        public int Altura { get; set; }
+
+        public int Peso { get; set; }
+
+
+        //relacionando item -->TipoItem
+        public int TipoItemID { get; set; }
+
+        public virtual TipoItem _TipoItem { get; set; }
+    }
+}
